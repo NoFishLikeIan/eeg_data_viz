272a12dfc16141259431ec303dc600fae03b7dce
NoFishLikeIan <andreatitton96@gmail.com>
2019-11-13 11:17:46 +0100

    🐍 added data explorer

--- data/data_parser.py
+++ data/data_parser.py
@@ -1,28 +1,38 @@
 import os
 import scipy.io
 import json
+import numpy as np
 
 
 def new_name(file):
     return f'{str(file).split(".")[0]}.json'
 
 
-def read_directory(directory='data/'):
+def read_directory(directory='data/', save=True):
     indirectory = os.path.join(directory, 'in')
+
+    collections_dataset = []
 
     for file in os.listdir(indirectory):
         if file.endswith(".mat"):
             filename = os.path.join(indirectory, str(file))
             mat = scipy.io.loadmat(filename)
 
-            outpath = os.path.join(directory, 'out', new_name(file))
-            listified = mat.get('av_subs').tolist()
+            dataset = mat.get('av_subs')
+            collections_dataset.append(dataset)
 
-            with open(outpath, 'w') as outfile:
-                json.dump(listified, outfile)
+            if save is True:
+                outpath = os.path.join(directory, 'out', new_name(file))
 
-                print('Saved in ', outpath)
+                listified = dataset.tolist()
+
+                with open(outpath, 'w') as outfile:
+                    json.dump(listified, outfile)
+
+                    print('Saved in ', outpath)
+
+    return collections_dataset
 
 
 if __name__ == '__main__':
-    read_directory()
+    dataset_collection = read_directory(save=False)
